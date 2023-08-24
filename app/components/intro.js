9c70581ec80ab77f6b79225452e7f40fab0f2a9c
jesslark <jesslarkdev@gmail.com>
2023-08-24 08:58:20 -0700

    fix error in shared component

--- app/components/intro.js
+++ app/components/intro.js
@@ -1,11 +1,11 @@
 export default function Intro() {
   return (
     <p>
-      So there are two kinds of routing, and I think the newer one is is the app
-      style (maybe new in v13?), which this app uses, but this{" "}
-      <em>particular</em> page is using the older style, the pages directory
-      style. Both can be used in the same app, for the purposes of being able to
-      migrate a little at a time, as demonstrated here.
+      There are two kinds of routing, and I think the newer one is is the app
+      style (maybe new in v13?), which this app uses by default, but it's also
+      using the older style, the pages directory style. Both can be used in the
+      same app, for the purposes of being able to migrate a little at a time, as
+      demonstrated here.
     </p>
   )
 }
